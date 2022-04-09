758decaf0e66d738a97efe549f0c0e4146f91612
joe <admin@egenesis.online>
2022-04-09 14:27:34 +0200

    send task success

--- step_one.py
+++ step_one.py
@@ -1,5 +1,15 @@
 import os
+import boto3
+
+client = boto3.client('stepfunctions')
 
 
 step_input = os.getenv("STEP_ONE", default="Default 1")
+task_token = os.getenv("TASK_TOKEN", default="TASK TOKEN PLACEHOLDER")
+
 print("this is step one with input: {i}.".format(i=step_input))
+
+client.send_task_success(
+    taskToken=task_token,
+    output='task one completed successfully'
+)
