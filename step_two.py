8a3175ee388222baf3bf996b505dca9a86b89c55
joe <admin@egenesis.online>
2022-04-09 07:59:34 +0200

    file op

--- step_two.py
+++ step_two.py
@@ -3,3 +3,5 @@ import os
 
 step_input = os.getenv("STEP_TWO", default="Default 2")
 print("this is step one with input: {i}.".format(i=step_input))
+with open("somefile.txt", 'r') as f:
+    f.read()
